e4152824e3b31606ec8642cbc40556f132f5e9ce
Mahammad <mbagirzade.01@gmail.com>
2023-07-29 16:23:18 +0400

    Jobs Page: Tag Component & Job Info

--- resources/views/job/index.blade.php
+++ resources/views/job/index.blade.php
@@ -1,7 +1,29 @@
 <x-layout>
     @foreach ($jobs as $job)
         <x-card class="mb-4">
-            {{ $job->title }}
+            <div class="mb-4 flex justify-between">
+                <h2 class="text-lg font-medium">{{ $job->title }}</h2>
+                <div class="text-slate-500">
+                    {{ $job->salary }} AZN
+                </div>
+            </div>
+
+            <div class="mb-4 flex items-center justify-between text-sm text-slate-500">
+                <div class="flex space-x-1">
+                    <div>Şirkət Adı &rarr;</div>
+                    <div>{{ $job->location }}</div>
+                </div>
+                <div class="flex space-x-1 text-xs">
+                    <x-tag>
+                        {{ Str::ucfirst($job->experience) }}
+                    </x-tag>
+                    <x-tag>{{ $job->category }}</x-tag>
+                </div>
+            </div>
+
+            <p class="text-sm text-slate-500">
+                {!! nl2br(e($job->description)) !!}
+            </p>
         </x-card>
     @endforeach
 </x-layout>
