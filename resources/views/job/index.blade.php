8f9db1cf35631936a94b389492c48c317f827353
Mahammad <mbagirzade.01@gmail.com>
2023-07-31 16:29:03 +0400

    Filtering Jobs: Radio Button Filters (Select One Option of Many)

--- resources/views/job/index.blade.php
+++ resources/views/job/index.blade.php
@@ -8,17 +8,45 @@
                 <div>
                     <div class="mb-1 font-semibold">Axtar</div>
                     <x-text-input name="search" value="{{ request('search') }}"
-                                  placeholder="İstənilən mətni axtarın" />
+                                  placeholder="Sayt üzrə axtarış" />
                 </div>
                 <div>
-                    <div class="mb-1 font-semibold">Əmək Haqqı</div>
+                    <div class="mb-1 font-semibold">Maaş</div>
 
                     <div class="flex space-x-2">
-                        <x-text-input name="min_salary" value="{{ request('min_salary') }}" placeholder="Min" />
-                        <x-text-input name="max_salary" value="{{ request('max_salary') }}" placeholder="Maks" />
+                        <x-text-input name="min_salary" value="{{ request('min_salary') }}"
+                                      placeholder="Min" />
+                        <x-text-input name="max_salary" value="{{ request('max_salary') }}"
+                                      placeholder="Maks" />
                     </div>
                 </div>
-                <div>3</div>
+                <div>
+                    <div class="mb-1 font-semibold">Təcrübə</div>
+
+                    <label for="experience" class="mb-1 flex items-center">
+                        <input type="radio" name="experience" value=""
+                            @checked(!request('experience')) />
+                        <span class="ml-2">Hamısı</span>
+                    </label>
+
+                    <label for="experience" class="mb-1 flex items-center">
+                        <input type="radio" name="experience" value="junior"
+                            @checked('junior' === request('experience')) />
+                        <span class="ml-2">Junior</span>
+                    </label>
+
+                    <label for="experience" class="mb-1 flex items-center">
+                        <input type="radio" name="experience" value="middle"
+                            @checked('middle' === request('experience')) />
+                        <span class="ml-2">Middle</span>
+                    </label>
+
+                    <label for="experience" class="mb-1 flex items-center">
+                        <input type="radio" name="experience" value="senior"
+                            @checked('senior' === request('experience')) />
+                        <span class="ml-2">Senior</span>
+                    </label>
+                </div>
                 <div>4</div>
             </div>
 
