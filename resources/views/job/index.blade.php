269ac87402bd940a9f80b92a68274a303c52d879
Mahammad <mbagirzade.01@gmail.com>
2023-07-31 14:47:33 +0400

    Filtering Jobs: Min & Max Salary

--- resources/views/job/index.blade.php
+++ resources/views/job/index.blade.php
@@ -7,14 +7,15 @@
             <div class="mb-4 grid grid-cols-2 gap-4">
                 <div>
                     <div class="mb-1 font-semibold">Axtar</div>
-                    <x-text-input name="search" value="" placeholder="İstənilən mətni axtarın" />
+                    <x-text-input name="search" value="{{ request('search') }}"
+                                  placeholder="İstənilən mətni axtarın" />
                 </div>
                 <div>
                     <div class="mb-1 font-semibold">Əmək Haqqı</div>
 
                     <div class="flex space-x-2">
-                        <x-text-input name="min_salary" value="" placeholder="Min" />
-                        <x-text-input name="max_salary" value="" placeholder="Maks" />
+                        <x-text-input name="min_salary" value="{{ request('min_salary') }}" placeholder="Min" />
+                        <x-text-input name="max_salary" value="{{ request('max_salary') }}" placeholder="Maks" />
                     </div>
                 </div>
                 <div>3</div>
