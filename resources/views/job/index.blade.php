72770784d424b82ddd513c0fc90c2a4d9823512b
Mahammad <mbagirzade.01@gmail.com>
2023-09-11 12:14:58 +0400

    Filtering Jobs: Clearing the Input

--- resources/views/job/index.blade.php
+++ resources/views/job/index.blade.php
@@ -3,21 +3,21 @@
                    :links="['İşlər' => route('jobs.index')]" />
 
     <x-card class="mb-4 text-sm">
-        <form action="{{ route('jobs.index') }}" method="GET">
+        <form id="filtering-form" action="{{ route('jobs.index') }}" method="GET">
             <div class="mb-4 grid grid-cols-2 gap-4">
                 <div>
                     <div class="mb-1 font-semibold">Axtar</div>
                     <x-text-input name="search" value="{{ request('search') }}"
-                                  placeholder="Sayt üzrə axtarış" />
+                                  placeholder="Sayt üzrə axtarış" form-id="filtering-form" />
                 </div>
                 <div>
                     <div class="mb-1 font-semibold">Maaş</div>
 
                     <div class="flex space-x-2">
                         <x-text-input name="min_salary" value="{{ request('min_salary') }}"
-                                      placeholder="Min" />
+                                      placeholder="Min" form-id="filtering-form" />
                         <x-text-input name="max_salary" value="{{ request('max_salary') }}"
-                                      placeholder="Maks" />
+                                      placeholder="Maks" form-id="filtering-form" />
                     </div>
                 </div>
                 <div>
