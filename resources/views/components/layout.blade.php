a8203672841a878589b1958f505c56f872028d2b
Mahammad <mbagirzade.01@gmail.com>
2023-10-02 12:35:42 +0400

    Authentication: Sign-in Logic

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -10,6 +10,7 @@
 </head>
 
 <body class="from-10% via-30% to-90% mx-auto mt-10 max-w-2xl bg-gradient-to-r from-indigo-100 via-sky-100 to-emerald-100 text-slate-700">
+    {{ auth()->user()->name ?? 'Qonaq'}}
     {{ $slot }}
 </body>
 
