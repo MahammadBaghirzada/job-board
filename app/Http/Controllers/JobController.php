0c8d424e2750cd12092bb036eadd3a75af365ab0
Mahammad <mbagirzade.01@gmail.com>
2023-10-02 10:51:40 +0400

    Employer: Other Employer Jobs on the Job Page

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -47,7 +47,9 @@ class JobController extends Controller
      */
     public function show(Job $job)
     {
-        return view('job.show', ['job' => $job->load('employer')]);
+        return view(
+            'job.show',
+            ['job' => $job->load('employer.jobs')]);
     }
 
     /**
