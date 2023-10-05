7607bc969b64a73e8c3c5f7bc593d1f20ca7f205
Mahammad <mbagirzade.01@gmail.com>
2023-10-05 14:57:34 +0400

    My Applications: Applicant Count, Average Asking Salary

--- resources/views/my_job_application/index.blade.php
+++ resources/views/my_job_application/index.blade.php
@@ -3,7 +3,26 @@
                    :links="['İş Müraciətlərim' => '#']" />
 
     @forelse($applications as $application)
-        <x-job-card :job="$application->job"></x-job-card>
+        <x-job-card :job="$application->job">
+            <div class="flex items-center justify-between text-xs text-slate-500">
+                <div>
+                    <div>
+                        {{ $application->created_at->diffForHumans() }} müraciət olunub
+                    </div>
+                    <div>
+                        Digər {{ Str::plural('istifadəçilər:', $application->job->job_applications_count - 1) }}
+                        {{ $application->job->job_applications_count - 1 }}
+                    </div>
+                    <div>
+                        İstədiyiniz maaş {{number_format($application->expected_salary)}} AZN-dir
+                    </div>
+                    <div>
+                        Ortalama əmək haqqı {{ number_format($application->job->job_applications_avg_expected_salary) }} AZN-dir
+                    </div>
+                </div>
+                <div></div>
+            </div>
+        </x-job-card>
     @empty
     @endforelse
 </x-layout>
