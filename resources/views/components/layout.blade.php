61149feee6e9cba0319e678c622c265187b38c97
Mahammad <mbagirzade.01@gmail.com>
2023-10-09 12:06:53 +0400

    Refactor Time: Flash Errors

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -40,12 +40,18 @@
         </ul>
     </nav>
 
-
     @if (session('success'))
         <div role="alert"
              class="my-8 rounded-md border-l-4 border-green-300 bg-green-100 p-4 text-green-700 opacity-75">
             <p class="font-bold">Uğurlu!</p>
             <p>{{ session('success') }}</p>
+        </div>
+    @endif
+    @if (session('error'))
+        <div role="alert"
+             class="my-8 rounded-md border-l-4 border-red-300 bg-red-100 p-4 text-red-700 opacity-75">
+            <p class="font-bold">Xəta!</p>
+            <p>{{ session('error') }}</p>
         </div>
     @endif
 
