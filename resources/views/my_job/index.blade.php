7e3ba2b6beb9234d1ed9abb133a48f4bf0244ecb
Mahammad <mbagirzade.01@gmail.com>
2023-10-11 15:20:10 +0400

    Employer: Job List

--- resources/views/my_job/index.blade.php
+++ resources/views/my_job/index.blade.php
@@ -1,5 +1,41 @@
 <x-layout>
-    <x-card>
-        All Jobs!
-    </x-card>
+    <x-breadcrumbs :links="['Mənim İşlərim' => '#']" class="mb-4" />
+
+    <div class="mb-8 text-right">
+        <x-link-button href="{{ route('my-jobs.create') }}">Yeni İş</x-link-button>
+    </div>
+
+    @forelse ($jobs as $job)
+        <x-job-card :$job>
+            <div class="text-xs text-slate-500">
+                @forelse ($job->jobApplications as $application)
+                    <div class="mb-4 flex items-center justify-between">
+                        <div>
+                            <div>{{ $application->user->name }}</div>
+                            <div>
+                                {{ $application->created_at->diffForHumans() }} müraciət edilib
+                            </div>
+                            <div>
+                                CV yükləyin
+                            </div>
+                        </div>
+
+                        <div>{{ number_format($application->expected_salary) }} AZN</div>
+                    </div>
+                @empty
+                    <div>Müraciət yoxdur</div>
+                @endforelse
+            </div>
+        </x-job-card>
+    @empty
+        <div class="rounded-md border border-dashed border-slate-300 p-8">
+            <div class="text-center font-medium">
+                İş yoxdur
+            </div>
+            <div class="text-center">
+                İlk işinizi <a class="text-indigo-500 hover:underline"
+                               href="{{ route('my-jobs.create') }}">burada </a>yerləşdirin!
+            </div>
+        </div>
+    @endforelse
 </x-layout>
